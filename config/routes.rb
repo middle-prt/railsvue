48ee870a84158b00e4714f0a9d3c215bde0ac70a
hayatesan <hayate.sh.999@gmail.com>
2019-06-07 23:43:36 +0900

    Make any

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+  resources :home
   # APIコントローラへのルーティング
   namespace :api, {format: 'json'} do
     namespace :v1 do
-      resources :prmaps, only: [:index]
+      resources :prmaps, only: [ :index]
     end
   end
 end
